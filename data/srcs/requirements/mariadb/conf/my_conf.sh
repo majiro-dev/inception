afed7fc5ac46dddef4d1d11954c3cb82f1d2ddd8
majiro1996 <majiro1996@gmail.com>
2023-11-20 17:23:03 +0100

    wordpress

--- data/srcs/requirements/mariadb/conf/my_conf.sh
+++ data/srcs/requirements/mariadb/conf/my_conf.sh
@@ -1,9 +1,9 @@
 
 service mysql start;
 
-mysql -e "CREATE DATABASE IF NOT EXITS \`${SQL_DATABASE}\`;"
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
 
-mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\'@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
 
 mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
 
