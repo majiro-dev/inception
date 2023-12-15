a1d2f7ea995816e5268390744f0698d65dfceb58
majiro1996 <majiro1996@gmail.com>
2023-12-15 11:12:02 +0100

    wordpress cli working

--- srcs/requirements/mariadb/conf/my_conf.sh
+++ srcs/requirements/mariadb/conf/my_conf.sh
@@ -12,7 +12,7 @@ mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
 mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
 mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
 
-mysql -e "DROP USER IF EXISTS ''@'localhost';"
+#mysql -e "DROP USER IF EXISTS ''@'localhost';"
 
 # Flush the privileges and shutdown the MariaDB server
 mysql -e "FLUSH PRIVILEGES;"
--- srcs/requirements/wordpress/conf/my_conf.sh
+++ srcs/requirements/wordpress/conf/my_conf.sh
@@ -1,12 +1,18 @@
 #!bin/bash
 sleep 10
+
+wp-cli.phar cli update --yes --allow-root
+wp-cli.phar core download --allow-root
+
 if [ ! -e /var/www/wordpress/wp-config.php ]; then
-    wp config create	--allow-root --dbname=$SQL_DATABASE --dbuser=$SQL_USER --dbpass=$SQL_PASSWORD \
+    wp-cli.phar config create	--allow-root --dbname=$SQL_DATABASE --dbuser=$SQL_USER --dbpass=$SQL_PASSWORD \
     					--dbhost=mariadb:3306 --path='/var/www/wordpress'
 
 sleep 2
-wp core install     --url=$DOMAIN_NAME --title=$SITE_TITLE --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASSWORD --admin_email=$ADMIN_EMAIL --allow-root --path='/var/www/wordpress'
-wp user create      --allow-root --role=author $USER1_LOGIN $USER1_MAIL --user_pass=$USER1_PASS --path='/var/www/wordpress' >> /log.txt
+#echo "CORE INSTALL STARTING"
+wp-cli.phar core install     --allow-root --url=$DOMAIN_NAME --title=$SITE_TITLE --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASSWORD --admin_email=$ADMIN_EMAIL --path='/var/www/wordpress' >> /log.txt 2>&1
+#echo "USER CREATION STARTING"
+wp-cli.phar user create      --allow-root --role=author $USER1_LOGIN $USER1_MAIL --user_pass=$USER1_PASS --path='/var/www/wordpress' >> /log.txt 2>&1
 fi
 
 
