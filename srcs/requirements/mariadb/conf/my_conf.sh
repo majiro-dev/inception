0c4f20e8330066953a65bdf72195bea367dcad07
majiro1996 <majiro1996@gmail.com>
2024-01-02 19:17:40 +0100

    mariadb my_conf using password for further configuration

--- srcs/requirements/mariadb/conf/my_conf.sh
+++ srcs/requirements/mariadb/conf/my_conf.sh
@@ -1,21 +1,21 @@
 # Start the MariaDB server with the --skip-networking option so it doesn't accept connections
-mysqld_safe --skip-networking &
+mysqld_safe &
 
 # Wait for the MariaDB server to start
 sleep 10
 
 # Change the root password
-#mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
 
 # Create the database and user
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
-mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -uroot -p"${SQL_ROOT_PASSWORD}" -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+mysql -uroot -p"${SQL_ROOT_PASSWORD}" -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -uroot -p"${SQL_ROOT_PASSWORD}" -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
 
-#mysql -e "DROP USER IF EXISTS ''@'localhost';"
+mysql -uroot -p"${SQL_ROOT_PASSWORD}" -e "DROP USER IF EXISTS ''@'localhost';"
 
 # Flush the privileges and shutdown the MariaDB server
-mysql -e "FLUSH PRIVILEGES;"
+mysql -uroot -p"${SQL_ROOT_PASSWORD}" -e "FLUSH PRIVILEGES;"
 mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
 
 # Start the MariaDB server normally
