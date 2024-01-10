09350fecb921a6c3cbd0ec7b50be504d1b5bf920
majiro1996 <majiro1996@gmail.com>
2024-01-10 12:43:52 -0500

    further cleaning

--- srcs/requirements/wordpress/conf/my_conf.sh
+++ srcs/requirements/wordpress/conf/my_conf.sh
@@ -5,14 +5,29 @@ wp-cli.phar cli update --yes --allow-root
 wp-cli.phar core download --allow-root
 
 if [ ! -e /var/www/wordpress/wp-config.php ]; then
-    wp-cli.phar config create	--allow-root --dbname=$SQL_DATABASE --dbuser=$SQL_USER --dbpass=$SQL_PASSWORD \
-    					--dbhost=mariadb:3306 --path='/var/www/wordpress'
+    wp-cli.phar config create	\
+	    --allow-root \
+	    --dbname=$SQL_DATABASE \
+	    --dbuser=$SQL_USER \
+	    --dbpass=$SQL_PASSWORD \
+	    --dbhost=mariadb:3306 --path='/var/www/wordpress'
 
 sleep 2
 #echo "CORE INSTALL STARTING"
-wp-cli.phar core install     --allow-root --url=$DOMAIN_NAME --title=$SITE_TITLE --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASSWORD --admin_email=$ADMIN_EMAIL --path='/var/www/wordpress' >> /log.txt 2>&1
+wp-cli.phar core install    \
+   	--allow-root \
+   	--url=$DOMAIN_NAME \
+   	--title=$SITE_TITLE \
+   	--admin_user=$ADMIN_USER \
+   	--admin_password=$ADMIN_PASSWORD \
+   	--admin_email=$ADMIN_EMAIL \
+   	--path='/var/www/wordpress' >> /log.txt 2>&1
 #echo "USER CREATION STARTING"
-wp-cli.phar user create      --allow-root --role=author $USER1_LOGIN $USER1_MAIL --user_pass=$USER1_PASS --path='/var/www/wordpress' >> /log.txt 2>&1
+wp-cli.phar user create  \
+	--allow-root \
+	--role=author $USER1_LOGIN $USER1_MAIL \
+	--user_pass=$USER1_PASS \
+	--path='/var/www/wordpress' >> /log.txt 2>&1
 fi
 
 
