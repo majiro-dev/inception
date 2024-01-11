0109e0582fe43d4f1c6fe99e04ccffbf63777382
majiro1996 <majiro1996@gmail.com>
2024-01-11 07:54:23 -0500

    removed comments

--- srcs/requirements/mariadb/conf/my_conf.sh
+++ srcs/requirements/mariadb/conf/my_conf.sh
@@ -1,23 +1,19 @@
-# Start the MariaDB server with the --skip-networking option so it doesn't accept connections
+
 mysqld_safe &
 
 # Wait for the MariaDB server to start
 sleep 10
 
-# Change the root password
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
 
-# Create the database and user
 mysql -uroot -p"${SQL_ROOT_PASSWORD}" -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
 mysql -uroot -p"${SQL_ROOT_PASSWORD}" -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
 mysql -uroot -p"${SQL_ROOT_PASSWORD}" -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
 
 mysql -uroot -p"${SQL_ROOT_PASSWORD}" -e "DROP USER IF EXISTS ''@'localhost';"
 
-# Flush the privileges and shutdown the MariaDB server
 mysql -uroot -p"${SQL_ROOT_PASSWORD}" -e "FLUSH PRIVILEGES;"
 mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
 
-# Start the MariaDB server normally
 exec mysqld_safe
 
